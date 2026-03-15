38e76ca29b31e89e8da08fbb4211b97cc520eee4
vijay <krishh24997@gmail.com>
2026-03-15 14:22:39 +0530

    Chnages made

--- workflows/pyspark_dag.py
+++ workflows/pyspark_dag.py
@@ -10,10 +10,10 @@ from airflow.providers.google.cloud.operators.dataproc import (
 )
 
 # define the variables
-PROJECT_ID = "avd-databricks-demo"
-REGION = "us-east1"
-CLUSTER_NAME = "my-demo-cluster"
-COMPOSER_BUCKET = "us-central1-demo-instance-a649b781-bucket" ## change this 
+PROJECT_ID = "gcp-new-1628"
+REGION = "us-Central1"
+CLUSTER_NAME = "my-demo-cluster2"
+COMPOSER_BUCKET = "us-central1-demo-instance-4eb986ec-bucket" ## change this 
 
 GCS_JOB_FILE_1 = f"gs://{COMPOSER_BUCKET}/data/INGESTION/hospitalA_mysqlToLanding.py"
 PYSPARK_JOB_1 = {
@@ -45,7 +45,7 @@ PYSPARK_JOB_4 = {
 
 
 ARGS = {
-    "owner": "SHAIK SAIDHUL",
+    "owner": "vijay krishna",
     "start_date": None,
     "depends_on_past": False,
     "email_on_failure": False,
